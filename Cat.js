f1d87058543e0888a90d96be09c3ce7cc22a0572
byenet <nhathuynguyenho@gmail.com>
2020-04-27 17:58:24 +0700

    lan1

--- Cat.js
+++ Cat.js
@@ -1,5 +1,14 @@
+var Mouse = require('./Mouse');
 function Cat(){
-    this.name = name;
+    this.stomach = [];
 }
+
+Cat.prototype.eat = function(animal) {
+    if(animal instanceof(Mouse)){
+        this.stomach.push(animal);
+    } else {
+        throw new Error('Cat can only eat mouse');
+    }
+};
 
 module.exports = Cat;
